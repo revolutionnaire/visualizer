014e54b5a64292e04d4d4ec5cb215909c535be34
Samuel Bacay <smlbcy@gmail.com>
2019-01-25 20:17:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Visualizer
+==========
+
+Drag a sound file to the yellow dot and watch it dance. Enjoy!
+
+Type `bundle exec jekyll serve` to start.
+
+#### License
+This page is published under [**Creative Commons**](/LICENSE)
---
+++ assets/javascripts/visualizer.js
@@ -0,0 +1,247 @@
+/* http://revolutionnaire.github.io/
+   v0.1.1 | 20190112
+   License: none (public domain)
+*/
+
+var visualizer;
+
+document.addEventListener('DOMContentLoaded', function(e) {
+
+  visualizer = new Visualizer();
+  visualizer.initialize();
+  visualizer.createBall();
+  visualizer.setupAudioProcessing();
+  visualizer.handleDrop()
+
+});
+
+function Visualizer () {
+
+    //Constant
+    this.sphereRadius = 6;
+
+    //Rendering
+    this.camera;
+    this.controls;
+    this.renderer;
+    this.scene;
+
+    //Ball
+    this.ball;
+
+    //Audio
+    this.amplitude = 1.8;
+    this.analyser;
+    this.audio;
+    this.audioContext;
+    this.noise;
+    this.javascriptNode;
+    this.sourceBuffer;
+
+}
+
+Visualizer.prototype.initialize = function () {
+
+  // Setup audio to element
+  this.audio = document.getElementById('sound');
+
+  // Initialize simplex noise
+  this.noise = new SimplexNoise();
+
+  // Generate a three.js scene
+  this.scene = new THREE.Scene();
+
+  // Get the width and height
+  var vw = window.innerWidth,
+      vh = window.innerHeight;
+
+  // Get the renderer
+  this.renderer = new THREE.WebGLRenderer({antialias:true})
+  this.renderer.setSize(vw,vh);
+
+  // Add the renderer to the DOM
+  document.body.insertBefore(this.renderer.domElement, document.querySelector('script'));
+
+  // Create and add camera
+  this.camera = new THREE.PerspectiveCamera(40, vw/vh, 0.1, 20000);
+  this.camera.position.set(0, 45, 0);
+  this.scene.add(this.camera);
+
+  var that = this;
+
+  window.addEventListener('resize', function(e) {
+
+    var vw = window.innerWidth,
+    vh = window.innerHeight;
+
+    that.renderer.setSize(vw, vh);
+
+    that.camera.aspect = vw/vh;
+    that.camera.updateProjectMatrix();
+
+  });
+
+  // Background colour of the scene
+  this.renderer.setClearColor(0xFFFFFF, 1);
+
+  // Create a light and add it to the scene
+  var light = new THREE.PointLight(0xFFFFFF);
+  light.position.set(-100, 200, 100);
+  this.scene.add(light);
+
+  // Add interaction capabilities to the scene
+  this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+
+};
+
+Visualizer.prototype.createBall = function () {
+
+  // Create and add the sphere
+  var geometry = new THREE.SphereGeometry(this.sphereRadius, 32, 32);
+  var material = new THREE.MeshBasicMaterial({color: 0xFFFF00, wireframe: false});
+  this.ball = new THREE.Mesh(geometry, material);
+  this.scene.add(this.ball);
+
+};
+
+Visualizer.prototype.averageInArray = function(array) {
+  var total = array.reduce(function(sum, b) { return sum + b});
+  return (total / array.length);
+};
+
+Visualizer.prototype.maxInArray = function(array) {
+  return array.reduce(function(a, b) { return Math.max (a, b) });
+};
+
+Visualizer.prototype.fractionate = function(value, minValue, maxValue) {
+  return (value - minValue)/(maxValue - minValue);
+};
+
+Visualizer.prototype.modulate = function(value, minValue, maxValue, outterMin, outterMax) {
+  var fraction = this.fractionate(value, minValue, maxValue);
+  var delta = outterMax - outterMin;
+  return outterMin + (fraction * delta);
+};
+
+Visualizer.prototype.setupAudioProcessing = function () {
+
+  // Setup the audio context
+  this.audioContext = new AudioContext();
+
+  // Create a JS node
+  this.javascriptNode = this.audioContext.createScriptProcessor(2048, 1, 1);
+  this.javascriptNode.connect(this.audioContext.destination);
+
+  // Create the source buffer
+  this.sourceBuffer = this.audioContext.createMediaElementSource(this.audio);
+
+  // Create the analyser node
+  this.analyser = this.audioContext.createAnalyser();
+  this.analyser.smoothingTimeConstant = 0.3;
+  this.analyser.fftSize = 512;
+
+  // Connect source to analyser
+  this.sourceBuffer.connect(this.analyser);
+
+  // Then, the analyser to speakers
+  this.analyser.connect(this.javascriptNode);
+
+  // Connect source to analyser
+  this.sourceBuffer.connect(this.audioContext.destination);
+
+  var that = this
+
+  this.javascriptNode.addEventListener('audioprocess', function(e) {
+
+    // Get the average for the first channel
+    var array = new Uint8Array(that.analyser.frequencyBinCount);
+    that.analyser.getByteFrequencyData(array);
+
+    // Split the frequency between bass and treble
+    var bassArray = array.slice(0, (array.length/2) -1),
+        trebleArray = array.slice((array.length/2) -1, array.length -1);
+
+    // Compute
+    var lowerMaxFrequency = that.averageInArray(bassArray) / bassArray.length,
+        upperAverageFrequency = that.averageInArray(trebleArray) / trebleArray.length;
+
+    // Compute the bass and treble frequencies
+    var bassFrequency = that.modulate(Math.pow(lowerMaxFrequency, 0.5), 0, 1, 0, 8),
+        trebleFrequency = that.modulate(upperAverageFrequency, 0, 1, 0, 4);
+
+    // Render the scene and update controls
+    visualizer.renderer.render(visualizer.scene, visualizer.camera);
+    visualizer.controls.update();
+
+    visualizer.ball.geometry.vertices.forEach(function(vertex, i) {
+
+      var offset = visualizer.ball.geometry.parameters.radius;
+      vertex.normalize();
+
+      var distance = (offset + bassFrequency) + that.noise.noise3D(vertex.x, vertex.y, vertex.z) * that.amplitude * trebleFrequency;
+      vertex.multiplyScalar(distance);
+
+    });
+
+    visualizer.ball.geometry.verticesNeedUpdate = true;
+    visualizer.ball.geometry.normalsNeedUpdate = true;
+    visualizer.ball.geometry.computeVertexNormals();
+    visualizer.ball.geometry.computeFaceNormals();
+
+  });
+
+};
+
+Visualizer.prototype.start = function (file) {
+
+  // Set audio source
+  this.audio.src = URL.createObjectURL(file);
+
+  var label = document.getElementById('guide')
+
+  // Load audio
+  label.innerHTML = 'Loading';
+  this.audio.load();
+
+  // Play audio
+  this.audio.play();
+
+  // Place file name on label
+  var fileName = file.name.substring(0, file.name.length - 4);
+  label.innerHTML = fileName;
+
+};
+
+Visualizer.prototype.handleDrop = function () {
+
+  document.body.addEventListener('dragover', function(e) {
+
+    // Stop even from bubbling outside
+    e.stopPropagation;
+
+    // Stop from doing the normal reaction to event
+    e.preventDefault();
+
+    // Copy the audio to a new location
+    e.dataTransfer.dropEffect = 'copy';
+
+  }, false);
+
+  document.body.addEventListener('drop', function(e) {
+
+    // Stop even from bubbling outside
+    e.stopPropagation;
+
+    // Stop from doing the normal reaction to event
+    e.preventDefault();
+
+    // Get the file
+    var file = e.dataTransfer.files[0];
+
+    // Load the file
+    visualizer.audioContext.resume();
+    visualizer.start(file);
+
+  }, false);
+
+};
